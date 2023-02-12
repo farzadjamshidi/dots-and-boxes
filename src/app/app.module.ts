630f11e73d3ab3e0b87da046e1bf15e2707b888d
farzadjamshidi <farzadjamshidicom@yahoo.com>
2023-02-12 14:40:54 +0300

    Add home page and game board page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { GameBoardComponent } from './pages/game-board/game-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameBoardComponent
   ],
   imports: [
     BrowserModule,
