273e4fe4135319bc6c97ddf157ff9d026feb94bd
farzadjamshidi <farzadjamshidicom@yahoo.com>
2023-02-12 14:43:31 +0300

    Add routing for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameBoardComponent } from './pages/game-board/game-board.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'game-board',
+    component: GameBoardComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
