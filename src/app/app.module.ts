50af64762609d446ab0e9f72b038294f0e3588a3
farzadjamshidi <farzadjamshidicom@yahoo.com>
2023-02-12 15:05:03 +0300

    add start button to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './pages/home/home.component';
 import { GameBoardComponent } from './pages/game-board/game-board.component';
+import { HomeComponent } from './pages/home/home.component';
 
+const MATERIAL_MODULES = [
+  MatButtonModule
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +20,8 @@ import { GameBoardComponent } from './pages/game-board/game-board.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ...MATERIAL_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
